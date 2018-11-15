aaa488c76676680b439e213f29acc19ae2443df9
Mája <m.skola>
2018-11-15 15:16:15 +0100

    Tiskneme kruh.

---
+++ TisknemeKruh.java
@@ -0,0 +1,31 @@
+public class TisknemeKruh {
+    public static boolean vKruhu(int x, int y, int polomer) {
+        return polomer * polomer >= x * x + y * y;
+    }
+
+    public static void kruh (int polomer) {
+        for (int y = -polomer; y <= polomer; y++) {
+            for (int x = -polomer; x <= polomer; x++) {
+                if (vKruhu(x, y, polomer)) {
+                    System.out.print("X");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void ukaz(int polomer) {
+        System.out.printf("======\n %d \n", polomer);
+        kresli(polomer);
+        System.out.printf("\n\n");
+    }
+
+    public static void main(String[] args) {
+        ukaz(1);
+        ukaz(5);
+        ukaz(10);
+    }
+}
