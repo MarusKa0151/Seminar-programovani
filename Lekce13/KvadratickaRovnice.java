b5950b138d140d376535b25b05a49fdca7eeb965
Marie <m.skola>
2019-01-17 16:02:18 +0100

    Lekce14

--- Lekce13/KvadratickaRovnice.java
+++ Lekce13/KvadratickaRovnice.java
@@ -1,6 +1,14 @@
 //package cz.alisma.alej.prog;
-import cz.alisma.alej.prog.pg16.QuadraticEquationGenerator;
-import cz.alisma.alej.prog.pg16.QuadraticEquationPrinter;
+
+/*
+public interface QuadraticEquationPrinter {
+    public void printHeader(int count);
+    public void printEquation(int a, int b, int c, int x1, int x2);
+    public void printFooter();
+}
+*/
+import cz.alisma.alej.prog.pg13.QuadraticEquationGenerator;
+import cz.alisma.alej.prog.pg13.QuadraticEquationPrinter;
 
 public class KvadratickaRovnice {
     public static void main(String[] args) {
@@ -8,6 +16,10 @@ public class KvadratickaRovnice {
         // QuadraticEquationPrinter printer = new ScreenPrinter();
  
         QuadraticEquationGenerator.generate(10, printer);
+    }
+
+    public static class HtmlPrinter implements QuadraticEquationPrinter {
+        
     }
     
 }
